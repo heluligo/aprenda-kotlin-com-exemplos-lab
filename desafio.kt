937857b67f3ff40fc16210b703a6cec7a7c3368d
HELDER LUIS LIMA GONCALVES <104535343+heluligo@users.noreply.github.com>
2023-01-09 22:39:47 -0300

    Desafio concluido

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,50 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Aluno(val nome: String, val id: Long)
 
-class Usuario
+data class Cursos(val nome: String, val stack: String, val duracao: Int = 60)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Formacao(val nome: String, val conteudos: List<Cursos>, val nivel: Nivel) {
+    val inscritos = mutableListOf<Aluno>()
+        infix fun matricular(aluno: Aluno) { inscritos.add(aluno) }
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+            fun matricular(vararg alunos: Aluno){
+                for(aluno in alunos)
+                    inscritos.add(aluno)
+            }
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+            fun mostrarConteudos(){
+                for(conteudo in conteudos)
+                    println(conteudo.nome)
+            }
+
+            fun mostrarInscritos(){
+                for(inscrito in inscritos)
+                    println(inscrito.nome)
+            }
+        }
+
+        fun main() {
+        val helder = Aluno("Helder", 1L)
+        val denise = Aluno("Denise", 2L)
+        val marcelo = Aluno("João Marcelo", 3L)
+        val rhoanny = Aluno("Rhoanny", 4L)
+
+
+        val listaConteudos: List<Cursos> = listOf(
+            Cursos("Crie um app mobile","Android nativo"),
+            Cursos("Android com Kotlin - criando um app","Kotlin"),
+            Cursos("Boas práticas para criação de telas","Android nativo"),
+            Cursos("Kotlin - comunicação com Web API","Kotlin"))
+        val androidMobile = Formacao("Android Mobile", listaConteudos, Nivel.BASICO)
+
+        androidMobile?.let{
+            androidMobile.matricular(denise, marcelo, rhoanny, helder)
+        }
+        
+        println("Formação: ${androidMobile.nome}\n\t\tConteúdos:")        
+        androidMobile.mostrarConteudos()
+        println(" ")
+        println("\t\tInscritos:")
+        androidMobile.mostrarInscritos()
     }
-}
-
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-}
